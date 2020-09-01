218139fe823d8364e4540eed23e543f3c297787e
GeekTarget <59028035+GeekTarget@users.noreply.github.com>
2020-09-01 19:18:18 +0400

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
     dp.loop.create_task(handlers.new_film_get())
     dp.loop.create_task(handlers.rambler_news_get())
     dp.loop.create_task(handlers.igromania_get())
-    dp.loop.create_task(handlers.habr_get())
     dp.loop.create_task(handlers.get_new_music())
+    dp.loop.create_task(handlers.habr_get())
+    
     executor.start_polling(dp, skip_updates=True)
