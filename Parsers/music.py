09e6ea021a2d981c0b06ca06d0f325adc65d4f70
GeekTarget <59028035+GeekTarget@users.noreply.github.com>
2020-09-01 21:26:53 +0400

    Update music.py

--- Parsers/music.py
+++ Parsers/music.py
@@ -1,18 +1,12 @@
 import base64
 import requests
 from bs4 import BeautifulSoup
-import lxml
-
 
 Id = '402480'
 HEADERS = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-    'Cache-Control': 'max-age=0',
-    'Connection': 'keep-alive',
-    'Cookie': '_ym_d=1598363041; _ym_uid=1598363041163280418',
-    'Host': 'm.sefon.cc',
     'Upgrade-Insecure-Requests': '1',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36'
 }
@@ -42,10 +36,9 @@ def decrypt_url(data: str, key: str) -> str:
 def get_main():
     global r
     r = requests.get('https://ru.sefon.cc/news/', headers=HEADERS)
-    soup = BeautifulSoup(r.text, 'lxml')
-    new_music = soup.find('div class_="b_list_mp3s _ "').find('div', class_='mp3')
+    soup = BeautifulSoup(r.text, 'html.parser')
+    new_music = soup.find('div', class_="b_list_mp3s _").find('div', class_='mp3')
     return new_music
-
 
 def get_content():
     music = get_main()
@@ -64,15 +57,6 @@ def get_content():
 
 
 def get_id():
-    id = get_main().get('data-mp3_id')
+    music = get_main()
+    id = music.get('data-mp3_id')
     return id
-
-# while True:
-#     if get_id() == Id:
-#         print('NOT')
-#         time.sleep(10)
-#         session.close()
-#     else:
-#         Id = get_id()
-#         print(get_content())
-#         session.close()
