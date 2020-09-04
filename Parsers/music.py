bc001253b3cbeda7467c7dd1716d448122ec25a9
GeekTarget <59028035+GeekTarget@users.noreply.github.com>
2020-09-04 21:34:32 +0400

    Update music.py

--- Parsers/music.py
+++ Parsers/music.py
@@ -4,14 +4,6 @@ from bs4 import BeautifulSoup
 
 Id = '402480'
 HEADERS = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-    'Cache-Control': 'max-age=0',
-    'Connection': 'keep-alive',
-    'Cookie': '_ym_d=1598363041; _ym_uid=1598363041163280418',
-    'Host': 'm.sefon.cc',
-    'Upgrade-Insecure-Requests': '1',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
 }
 
