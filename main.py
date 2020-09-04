c1d385c2b0e89afcf168f465eee0eea16c9b6b9b
GeekTarget <59028035+GeekTarget@users.noreply.github.com>
2020-09-04 23:04:06 +0400

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,10 @@ dp = Dispatcher(bot)
 # Running bot
 if __name__ == '__main__':
     from handlers import dp
-    print(1)
-    dp.loop.create_task(handlers.get_new_music())
-    print(1)
-    #dp.loop.create_task(handlers.new_film_get())
-    #dp.loop.create_task(handlers.rambler_news_get())
-    #dp.loop.create_task(handlers.igromania_get())
-    #dp.loop.create_task(handlers.habr_get())
+    #dp.loop.create_task(handlers.get_new_music())
+    dp.loop.create_task(handlers.new_film_get())
+    dp.loop.create_task(handlers.rambler_news_get())
+    dp.loop.create_task(handlers.igromania_get())
+    dp.loop.create_task(handlers.habr_get())
         
     executor.start_polling(dp, skip_updates=True)
