2a6f5d3609d5ee62ee0ac8494f70dbc5ffa13f0a
GeekTarget <59028035+GeekTarget@users.noreply.github.com>
2020-09-05 08:49:04 +0400

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,7 @@ dp = Dispatcher(bot)
 if __name__ == '__main__':
     from handlers import dp
     #dp.loop.create_task(handlers.get_new_music())
-    dp.loop.create_task(handlers.new_film_get())
-    dp.loop.create_task(handlers.rambler_news_get())
-    dp.loop.create_task(handlers.igromania_get())
+ 
     dp.loop.create_task(handlers.habr_get())
         
     executor.start_polling(dp, skip_updates=True)
