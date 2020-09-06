135faa35aeb3f9572615eed86ea66365a9653494
GeekTarget <59028035+GeekTarget@users.noreply.github.com>
2020-09-06 10:46:22 +0400

    Update music.py

--- Parsers/music.py
+++ Parsers/music.py
@@ -40,8 +40,10 @@ def decrypt_url(data: str, key: str) -> str:
 def get_main():
     global r
     r = requests.get('https://ru.sefon.cc/news/',headers=HEADERS)
-    print(r.text)
-    soup = BeautifulSoup(r.text, 'html.parser')
+    session = requests.Session()
+    req = session.get('https://ru.sefon.cc/news/',headers=HEADERS)
+    print(req)
+    soup = BeautifulSoup(req.text, 'html.parser')
     print(soup)
     new_music = soup.find('div', class_="b_list_mp3s _").find('div', class_='mp3')
     return new_music
