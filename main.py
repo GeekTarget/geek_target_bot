47638b041359bb4c4dca75fe6af5932b6675f9e4
GeekTarget <59028035+GeekTarget@users.noreply.github.com>
2020-09-08 09:53:49 +0400

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.token)
 dp = Dispatcher(bot)
 
-# Running bot
+
 if __name__ == '__main__':
     from handlers import dp
-    dp.loop.create_task(handlers.get_new_music())
-#     dp.loop.create_task(handlers.new_film_get())
-#     dp.loop.create_task(handlers.rambler_news_get())
-#     dp.loop.create_task(handlers.igromania_get())
-#     dp.loop.create_task(handlers.habr_get())
+    #dp.loop.create_task(handlers.get_new_music()) 
+    dp.loop.create_task(handlers.new_film_get())
+    dp.loop.create_task(handlers.rambler_news_get())
+    dp.loop.create_task(handlers.igromania_get())
+    dp.loop.create_task(handlers.habr_get())
         
     executor.start_polling(dp, skip_updates=True)
